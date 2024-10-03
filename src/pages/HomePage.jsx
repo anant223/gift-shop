22dc529ffd835c31d86358b2300f8231a630b2b0
Anant <p.anant223@gmail.com>
2024-10-03 16:51:14 +0530

    fixing ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -98,7 +98,7 @@ const product = {
           <h2 className="text-3xl font-bold mb-8 text-center">
             Featured Gifts
           </h2>
-          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8 w-fit mx-auto ">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8 w-fit mx-auto lg:px-[90px]">
             <FeaturedCard
               image={product.image}
               price={product.price}
